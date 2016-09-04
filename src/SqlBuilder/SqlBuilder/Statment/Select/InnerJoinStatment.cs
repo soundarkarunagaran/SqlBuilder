abcbc3d21ff167d2b29ffa8455035fe3f6862155
wuyou331 <wuyou331@gmail.com>
2016-09-04 19:09:03 +0800

    实现了left join。

---
+++ src/SqlBuilder/SqlBuilder/Statment/Select/InnerJoinStatment.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlBuilder.Statment.Select
+{
+    public class InnerJoinStatment : StatementBase
+    {
+        internal InnerJoinStatment(SqlBuilder context, string tableName) : base(context)
+        {
+            this.StatementBock = string.Format("inner join {0}", tableName);
+        }
+
+        public WhereStatment Where(string condition)
+        {
+            return new WhereStatment(Context, condition);
+        }
+
+        public AsStatment As(string aliasName)
+        {
+            return new AsStatment(Context, aliasName);
+        }
+
+        public OrderByStatment OrderBy(params string[] columns)
+        {
+            return new OrderByStatment(Context, columns);
+        }
+
+        public GroupByStatment GroupBy(params string[] columns)
+        {
+            return new GroupByStatment(Context, columns);
+        }
+    }
+}
---
+++ src/SqlBuilder/SqlBuilder/Statment/Select/OnStatment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlBuilder.Statment.Select
+{
+  public  class OnStatment:StatementBase
+    {
+        internal OnStatment(SqlBuilder context, string condition) : base(context)
+        {
+            this.StatementBock = string.Format("on {0}", condition);
+        }
+
+        public WhereStatment Where(string condition)
+        {
+            return new WhereStatment(Context, condition);
+        }
+
+
+        public OrderByStatment OrderBy(params string[] columns)
+        {
+            return new OrderByStatment(Context, columns);
+        }
+
+        public GroupByStatment GroupBy(params string[] columns)
+        {
+            return new GroupByStatment(Context, columns);
+        }
+    }
+}
