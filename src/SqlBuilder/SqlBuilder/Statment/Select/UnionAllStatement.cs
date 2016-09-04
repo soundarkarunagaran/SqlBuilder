a64c0bece360505cf389f705f12dca1b7c89901e
wuyou331 <wuyou331@gmail.com>
2016-09-04 16:10:02 +0800

    增加Union操作。

---
+++ src/SqlBuilder/SqlBuilder/Statment/Select/UnionAllStatement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlBuilder.Statment.Select
+{
+    public class UnionAllStatement : UnionStatement
+    {
+        protected override string Symbol
+        {
+            get { return "union all"; }
+        }
+
+        internal UnionAllStatement(SqlBuilder context, StatementBase statmen) : base(context,statmen)
+        {
+        }
+    }
+}
