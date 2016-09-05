daa515cac421bc3e374b8481d70bc10faa6f60ed
wuyou331 <wuyou331@gmail.com>
2016-09-05 20:59:02 +0800

    增加Where条件方法。

---
+++ src/SqlBuilder/Statment/WhereCondition.cs
@@ -0,0 +1,20 @@
+﻿namespace SqlBuilder.Statment
+{
+    public static class WhereCondition
+    {
+        public static string In<T>(this string column, params T[] array)
+        {
+            return string.Format("{0} in ({1})", column, string.Join(",", array));
+        }
+
+        public static string In(this string column, StatementBase subStatment)
+        {
+            return string.Format("{0} in ({1})", column, subStatment);
+        }
+
+        public static string Exists(this string column, StatementBase subStatment)
+        {
+            return string.Format("{0} exists ({1})", column, subStatment);
+        }
+    }
+}
