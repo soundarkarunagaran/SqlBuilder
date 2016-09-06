22450fb5e02f5b168127f9ef8c2d612117f83a9f
wuyou331 <wuyou331@gmail.com>
2016-09-06 21:25:28 +0800

    is null.

--- src/SqlBuilder/Statment/WhereCondition.cs
+++ src/SqlBuilder/Statment/WhereCondition.cs
@@ -2,6 +2,16 @@
 {
     public static class WhereCondition
     {
+        public static string IsNull(this string column)
+        {
+            return string.Format("{0} is null", column);
+        }
+
+        public static string IsNotNull(this string column)
+        {
+            return string.Format("{0} is not null", column);
+        }
+
         public static string In<T>(this string column, params T[] array)
         {
             return string.Format("{0} in ({1})", column, string.Join(",", array));
